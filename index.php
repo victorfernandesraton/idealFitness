b7e9e68ac552695705d619e8cd3f11760e3b564f
Carlos Habib <43607843+CarHab@users.noreply.github.com>
2018-10-19 08:24:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Ideal Fitness</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section id="home">
+        <nav class="navbar navbar-expand-lg fixed-top">
+            <div class="container">
+                <a class="navbar-brand textoNav textoBrand" href="#"><span class="logo">I</span>deal<span class="logo"> F</span>itness</a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link textoNav" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link textoNav" href="#areaAluno">Área do Aluno</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link textoNav" href="#horarios">Horários</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link textoNav" href="#unidades">Unidades</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link textoNav" href="#sobre">Sobre</a>
+                            </li>
+                        </ul>
+                </div>
+            </div>
+        </nav>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 imgSliders" src="Imagens/slide1.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 imgSliders" src="Imagens/slide2.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100 imgSliders" src="Imagens/slide3.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <div class="row col-12">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="boxModalidades text-center col-12 col-md-3 float-left mt-5 mb-3">Musculação</div>
+                <div class="textModalidades col-12 col-sm-9 float-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+        <div class="row col-12">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="boxModalidades text-center col-12 col-md-3 float-right mt-5 mb-3">Crossfit</div>
+                <div class="textModalidades col-12 col-sm-9 float-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+        <div class="row col-12 mb-5">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="boxModalidades text-center col-12 col-md-3 float-left mt-5 mb-3">Fitdance</div>
+                <div class="textModalidades col-12 col-sm-9 float-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+    </section>
+    <section id="footer">
+        <div class="bgFooter">
+            assad
+        </div>
+    </section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
