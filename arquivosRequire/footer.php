4b6b66781ef5b4405aba4a204126a912cef32207
Carlos Habib <43607843+CarHab@users.noreply.github.com>
2018-10-20 18:55:37 -0300

    Add files via upload

---
+++ arquivosRequire/footer.php
@@ -0,0 +1,36 @@
+
+<section id="footer">
+            <div class="col-sm-10 col-lg-6 container">
+                <div class="row ml-3">
+                    <ul class="mt-4 col-6 footerUnidades">
+                        <li id="footerListHead">Unidades</li>
+                        <a href=""><li class="ml-2 text-light">Brotas</li></a> 
+                        <a href=""><li class="ml-2 text-light">Pituba</li></a> 
+                        <a href=""><li class="ml-2 text-light">Imbui</li></a> 
+                        <a href=""><li class="ml-2 text-light">Stiep</li></a> 
+                        <a href=""><li class="ml-2 text-light">Cajazeiras</li></a> 
+                    </ul>
+                        <div class="mt-4 col-6 container-fluid">
+                            <a href=""><h3 class="itemFooter">Área do Aluno</h3></a>
+                            <a href=""><h3 class="itemFooter mt-5">Freepass</h3></a>
+                        </div>
+                </div>
+                <div class="row float-right itemFooter mr-5 mr-lg-0 mr-xl-5 mt-2">
+                    <a href=""><img class="mb-5" src="/idealFitness/Imagens/instagram.png" alt="Icone Instagram"></a>
+                    <a href=""><img class="mb-5 ml-4" src="/idealFitness/Imagens/facebook.png" alt="Icone Facebook"></a>
+                    <h3 class="col-4 col-sm-2 offset-sm-0">Telefone: <br> <span class="numTel ml-1 text-light">(12)3456789</span></h3>
+                </div>
+            </div>
+    </section>
+    
+</body>
+
+<!-- SCRIPT JQUERY -->
+<script type="text/javascript" src=".\Frameworks\jquery\dist\jquery.min.js"></script>
+
+<!-- SCRIPT BOOTSTRAP -->
+<script type="text/javascript" src=".\Frameworks\bootstrap4\js\bootstrap.min.js"></script>
+    
+<!-- MEU SCRIPT -->
+<script type="text/javascript" src="./script/script.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ideal Fitness</title>
+    <?php require 'arquivosRequire/header.php'?>
+</head>
+<body>
+    <section id="home">
+        <?php require 'arquivosRequire/navbar.php'?>
+        <div id="carouselExampleControls" class="carousel slide mt-5" data-ride="carousel" data-interval="false">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="container btnsSlider">
+                        <div class="btnLaranja btn btn-lg ml-5 no-shadow">Ganhe Massa</div><br>
+                        <button class="btnLaranja btn btn-lg mt-3">Faça sua matrícula</button>
+                    </div>
+                    <img class="d-block w-100 imgSlider" src="Imagens/slide1.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <div class="container btnsSlider">
+                        <div class="btnEmagraca btnLaranja btn btn-lg ml-5 no-shadow">Emagreça</div><br>
+                        <button class="btnLaranja btn btn-lg mt-3">Faça sua matrícula</button>
+                    </div>
+                    <img class="d-block w-100 imgSlider" src="Imagens/slide2.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <div class="container btnsSlider">
+                        <div class="btnLaranja btn btn-lg ml-5 no-shadow">Seja Saudável</div><br>
+                        <button class="btnLaranja btn btn-lg mt-3">Faça sua matrícula</button>
+                    </div>
+                    <img class="d-block w-100 imgSlider" src="Imagens/slide3.png" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <div class="row col-12">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="btnLaranja boxModalidades text-center col-12 col-md-3 float-left mt-5 mb-3">Musculação</div>
+                <div class="textModalidades col-12 col-sm-9 float-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+        <div class="row col-12">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="btnLaranja boxModalidades text-center col-12 col-md-3 float-right mt-5 mb-3">Crossfit</div>
+                <div class="textModalidades col-12 col-sm-9 float-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+        <div class="row col-12 mb-5">
+            <div class="container-fluid ml-5 mt-5">
+                <div class="btnLaranja boxModalidades text-center col-12 col-md-3 float-left mt-5 mb-3">Fitdance</div>
+                <div class="textModalidades col-12 col-sm-9 float-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce a sapien eu lectus laoreet dignissim convallis at est. Quisque tempus leo in mauris mattis vehicula. 
+                    Morbi velit elit, viverra quis viverra sed, laoreet et mi. Aenean orci sem, aliquet sit amet turpis vel, 
+                    tempor sollicitudin tellus. Aliquam consectetur lacus sed nibh pharetra.</div>
+            </div>
+        </div>
+    </section>
+    <?php require 'arquivosRequire/footer.php'?>
+</body>
+</html>
